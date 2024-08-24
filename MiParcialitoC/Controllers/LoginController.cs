1b28f794fd84b7d68e8157993715b0544c8d86dc
Fernando Trejo <ferjo.trejo@gmail.com>
2024-08-24 13:35:09 -0600

    Deploy

---
+++ MiParcialitoC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiParcialitoC.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password, bool rememberMe)
+        {
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
